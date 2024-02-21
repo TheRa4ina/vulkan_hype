9a78beacce9644a891407fb4f2b377a2570bb07d
TheRa4ina <114075421+TheRa4ina@users.noreply.github.com>
2024-02-21 12:52:32 +0300

    Delete build/x64-Debug directory

--- build/x64-Debug/libhello/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/0main_stuff/prog_stuff/projects/cpp/vulkan_hype/libhello
-# Build directory: F:/0main_stuff/prog_stuff/projects/cpp/vulkan_hype/build/x64-Debug/libhello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("F:/0main_stuff/prog_stuff/projects/cpp/vulkan_hype/build/x64-Debug/libhello/HelloTests[1]_include.cmake")
--- build/x64-Debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/0main_stuff/prog_stuff/projects/cpp/vulkan_hype/src
-# Build directory: F:/0main_stuff/prog_stuff/projects/cpp/vulkan_hype/build/x64-Debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
